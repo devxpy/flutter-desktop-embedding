483bb9ee5d3576187e059446edfa024f025f6431
Dev Aggarwal <devxpy@gmail.com>
2019-10-28 18:24:34 +0530

    add generated files

--- example/linux/flutter/generated_plugin_registrant.cc
+++ example/linux/flutter/generated_plugin_registrant.cc
@@ -2,8 +2,11 @@
 //  Generated file. Do not edit.
 //
 
-#include "plugin_registrant.h"
+#include "generated_plugin_registrant.h"
 
+#include <file_chooser_plugin.h>
 
 void RegisterPlugins(flutter::PluginRegistry* registry) {
+  FileChooserPluginRegisterWithRegistrar(
+      registry->GetRegistrarForPlugin("FileChooserPlugin"));
 }
--- example/macos/Flutter/GeneratedPluginRegistrant.swift
+++ example/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,6 +5,10 @@
 import FlutterMacOS
 import Foundation
 
+import file_chooser
+import path_provider_fde
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  FileChooserPlugin.register(with: registry.registrar(forPlugin: "FileChooserPlugin"))
+  PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
 }
