f907aca7616e95de7654ac0532ad838f7f3da902
Dev Aggarwal <devxpy@gmail.com>
2019-10-28 18:15:33 +0530

    add example for pdfium

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,25 +1,23 @@
-// Copyright 2018 Google LLC
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//      http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
+import 'dart:ffi';
+import 'dart:typed_data';
+import 'dart:ui' as ui;
 
+import 'package:file_chooser/file_chooser.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/foundation.dart'
     show debugDefaultTargetPlatformOverride;
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
+import 'package:path_provider/path_provider.dart' as path;
+import 'package:pdfium/pdfium.dart';
 
-void main() {
+void main() async {
   // See https://github.com/flutter/flutter/wiki/Desktop-shells#target-platform-override
   debugDefaultTargetPlatformOverride = TargetPlatform.fuchsia;
+
+  WidgetsFlutterBinding.ensureInitialized();
+  var base = await path.getApplicationSupportDirectory();
+  loadDylib('${base.path}/debug/libpdfium.dylib');
+  initLibrary();
 
   runApp(new MyApp());
 }
@@ -34,54 +32,155 @@ class MyApp extends StatelessWidget {
         // See https://github.com/flutter/flutter/wiki/Desktop-shells#fonts
         fontFamily: 'Roboto',
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: Scaffold(
+        appBar: AppBar(title: Text('Flutter Pdfium')),
+        body: MyHomePage(),
+      ),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  final String title;
-
   @override
   _MyHomePageState createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+  var settingsVisible = true;
+  String selectedPdf;
 
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
+  @override
+  Widget build(BuildContext context) {
+    return ListView(
+      children: <Widget>[
+        ExpansionPanelList(
+          expansionCallback: (int index, bool isExpanded) {
+            setState(() {
+              settingsVisible = !settingsVisible;
+            });
+          },
+          children: [
+            ExpansionPanel(
+              canTapOnHeader: true,
+              headerBuilder: (context, isExpanded) {
+                return ListTile(title: Text('Settings'));
+              },
+              body: Column(
+                children: <Widget>[
+                  RaisedButton(
+                    child: Text('Choose PDF'),
+                    onPressed: () {
+                      showOpenPanel((result, files) {
+                        if (files.isEmpty) return;
+                        setState(() {
+                          selectedPdf = files[0];
+                        });
+                      }, allowedFileTypes: ['pdf']);
+                    },
+                  ),
+                  Text(
+                    selectedPdf ?? '',
+                    overflow: TextOverflow.ellipsis,
+                  ),
+                ],
+              ),
+              isExpanded: settingsVisible,
+            ),
+          ],
+        ),
+        Container(
+          child: PdfView(selectedPdf),
+        ),
+      ],
+    );
+  }
+}
+
+class PdfPainter extends CustomPainter {
+  final ui.Image image;
+
+  PdfPainter(this.image);
+
+  @override
+  void paint(ui.Canvas canvas, ui.Size size) {
+    canvas.drawCircle(Offset.zero, 20.0, Paint());
+    canvas.drawImage(image, Offset.zero, Paint());
+  }
+
+  @override
+  bool shouldRepaint(PdfPainter oldDelegate) {
+    return oldDelegate.image != image;
+  }
+}
+
+class PdfView extends StatefulWidget {
+  final String filePath;
+
+  PdfView(this.filePath);
+
+  @override
+  _PdfViewState createState() => _PdfViewState();
+}
+
+class _PdfViewState extends State<PdfView> {
+  ui.Image image;
+
+  @override
+  void didUpdateWidget(PdfView oldWidget) {
+    int width;
+    int height;
+    Pointer<FPDF_PAGE> page;
+    Pointer<FPDF_DOCUMENT> doc;
+    Pointer<FPDF_BITMAP> bitmap;
+    Uint8List buf;
+    int ppi = 100;
+
+    if (widget.filePath == null) return;
+
+    doc = loadDocument(widget.filePath);
+    page = fLoadPage(doc, 5);
+
+    width = fGetPageWidth(page).toInt();
+    height = fGetPageHeight(page).toInt();
+    width = pointsToPixels(width, ppi).toInt();
+    height = pointsToPixels(height, ppi).toInt();
+
+    bitmap = fBitmapCreate(width, height, 1);
+    fBitmapFillRect(bitmap, 0, 0, width, height, 0);
+    fRenderPageBitmap(bitmap, page, 0, 0, width, height, 0, 0);
+
+    buf = fBitmapGetBuffer(bitmap)
+        .asTypedList(width * height)
+        .buffer
+        .asUint8List();
+
+    ui.decodeImageFromPixels(
+      buf,
+      width,
+      height,
+      ui.PixelFormat.bgra8888,
+          (img) {
+        setState(() {
+          image = img;
+        });
+      },
+    );
+
+    fCloseDocument(doc);
+
+    super.didUpdateWidget(oldWidget);
   }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.display1,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ),
+    if (widget.filePath == null) {
+      return Container();
+    }
+    if (image == null) {
+      return CircularProgressIndicator();
+    }
+    return CustomPaint(
+      painter: PdfPainter(image),
     );
   }
 }
